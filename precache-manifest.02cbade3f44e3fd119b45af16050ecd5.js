00f2b48f9287e33a662eaca7d1fd2e7e2b89b6f3
Marcos Nicolau <marcosnicolau2003@gmail.com>
2021-02-21 21:33:25 -0300

    Updates

---
+++ precache-manifest.02cbade3f44e3fd119b45af16050ecd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fda48ac20b390144cd2633fb9a1670cc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5d19be7f346fdecb1142",
+    "url": "./static/css/main.5daa2040.chunk.css"
+  },
+  {
+    "revision": "f98534ccc88211bf74ba",
+    "url": "./static/js/2.70476f18.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.70476f18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d19be7f346fdecb1142",
+    "url": "./static/js/main.a2d46c8a.chunk.js"
+  },
+  {
+    "revision": "fb1261d712dffa6f5159",
+    "url": "./static/js/runtime-main.a37d1810.js"
+  }
+]);
